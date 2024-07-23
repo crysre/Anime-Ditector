5f1814c15a85f05e602e744d198354302d7ddc55
Tamojit Hazra <tamosrk@gmail.com>
2024-07-23 14:35:10 +0530

    added error handlig

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
 import express from "express";
 import bodyParser from "body-parser";
-import  axios from "axios";
+import axios from "axios";
 import multer from "multer";
 import path from "path";
 import fs from 'fs';
@@ -15,43 +14,45 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
 //using multer for the first time
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
+    destination: (req, file, cb) => {
         cb(null, 'uploads/');
     },
-    filename: (req, file, cb)=>{
+    filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
     }
 });
 const upload = multer({storage: storage});
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-app.get("/", (req, res)=>{
-    res.render("index")
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.post("/postUrlFile", upload.single('image'), async (req, res)=>{
-    const filePath = req.file.path
-    const formData = new FormData();
-    formData.append('image', fs.createReadStream(filePath));
+app.post("/postUrlFile", upload.single('image'), async (req, res) => {
+    try {
+        const filePath = req.file.path;
+        const formData = new FormData();
+        formData.append('image', fs.createReadStream(filePath));
 
-    const result = await axios.post("https://api.trace.moe/search", formData)
-    const data = result.data
-    
-    res.render("index", {data: data})
-})
+        const result = await axios.post("https://api.trace.moe/search", formData, {
+            headers: formData.getHeaders(),
+        });
+        const data = result.data;
 
+        res.render("index", {data: data});
+    } catch (error) {
+        // Log detailed error information
+        console.error("Error occurred:", error);
 
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port}`)
+        res.status(500).send("Internal Server Error");
+    }
 });
 
-
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
